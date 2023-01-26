e9f8cbb618f56760e88a11618ff4211644506f42
nithinnalla <97667378+NithinNalla@users.noreply.github.com>
2023-01-26 22:53:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-a=int(input())
-b=int(input())
-f=a*(b/100)+a
-print(round(f))
+a=input()
+b=input()
+if str(a).isdigit() and str(b).isdigit():
+    f=int(a)*(int(b)/100)+int(a)
+    print(round(f))
+else:
+    print("Invalid Input")
