afb200846ddc9f19946047a5ad764af7da22a7f5
nithinnalla <97667378+NithinNalla@users.noreply.github.com>
2023-01-26 22:28:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-a=input()
-if('.'in a):
-    k=round(float(a))
-else:
-    k=int(a)
-b=input()
-if('.'in a):
-    p=round(float(b))
-else:
-    p=int(b)
-f=k*(p/100)+k
-print(int(f))
+a=int(input())
+b=int(input())
+f=a*(b/100)+a
+print(round(f))
